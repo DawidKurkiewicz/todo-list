2c84190743289e96723e2d44ca13fe2b600a9efe
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-16 15:17:19 +0100

    added line-throught style

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+let completed = false
 
 
 const remove = () => {
     event.target.parentNode.remove()
 }
 
-document.querySelectorAll("li button").forEach( item => item.addEventListener("click", remove))
+const line = () => {
+    if (completed === true) {
+        completed = false
+        event.target.style.textDecoration = "none"
+    } else {
+        completed = true
+        event.target.style.textDecoration = "line-through"
+    }
+}
+
+document.querySelectorAll("li button").forEach(item => item.addEventListener("click", remove))
+document.querySelectorAll("li").forEach(item => item.addEventListener("click", line))
 
