f7dec0386c0bbfe242e3c9f09f808f6ae94487c0
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-16 15:49:25 +0100

    search using filter method

--- script.js
+++ script.js
@@ -16,4 +16,14 @@ const line = () => {
 
 document.querySelectorAll("li button").forEach(item => item.addEventListener("click", remove))
 document.querySelectorAll("li").forEach(item => item.addEventListener("click", line))
+const input = document.querySelector("input")
+const ul = document.querySelector("ul")
+const li = document.querySelectorAll("li")
 
+const search = (e) => {
+const searchText = e.target.value.toLowerCase()
+let elements = [...li];
+elements = elements.filter(li => li.textContent.toLowerCase().includes(searchText))
+}
+
+input.addEventListener("input", search)
